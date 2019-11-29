4afd1e4e57d886e113d38457e8c9b203df00474b
Jonas_Stix <jonas.stix@gmail.com>
2019-11-29 10:10:41 +0100

    Add files via upload

---
+++ report.h
@@ -0,0 +1,12 @@
+//
+// Created by patrick on 11/28/19.
+//
+
+#ifndef WARMING_UP_CPP_REPORT_H
+#define WARMING_UP_CPP_REPORT_H
+
+bool reportOpenFile(char *source);
+bool reportReport();
+bool reportCloseFile();
+
+#endif //WARMING_UP_CPP_REPORT_H
